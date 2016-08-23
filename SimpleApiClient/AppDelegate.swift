71e08445d7394148d4022d974d7ac55be97c2597
ShengHuaWu <fantasy0404@gmail.com>
2016-08-23 11:47:55 +0800

    Configure app transport security exception in info.plist. Write test api requets in AppDelegate.

--- SimpleApiClient/AppDelegate.swift
+++ SimpleApiClient/AppDelegate.swift
@@ -13,6 +13,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        let api = Api()
+//        api.getUserInfo("1qaz9ijn") { result in            
+//            do {
+//                let user = try result.unwrap()
+//                debugPrint(user)
+//            } catch {
+//                debugPrint(error)
+//            }
+//        }
+        
+        let user = User(userId: "1qaz9ijn", name: "joe lin", email: "joe.lin@gmail.com", description: "i am immortal joe.")
+        api.updateUserInfo(user) { result in
+            do {
+                let user = try result.unwrap()
+                debugPrint(user)
+            } catch {
+                debugPrint(error)
+            }
+        }
+        
         return true
     }
 }
